d2268fb093eb1a54536cd2b9d3a9d4745dad49d8
halchanar <177578634+halchanar@users.noreply.github.com>
2025-07-08 06:27:11 -0400

    Use `ruff` for code formatting

--- lib/guide.py
+++ lib/guide.py
@@ -11,12 +11,14 @@ def validate_journal_data(journal_data):
     try:
         if len(journal_data["headers"]) != 4:
             raise ValueError(
-                f"Journal data mismatch: {len(journal_data['headers'])} headers " +
-                "but expected 4")
+                f"Journal data mismatch: {len(journal_data['headers'])} headers "
+                + "but expected 4"
+            )
         elif len(journal_data["levels"]) != 3:
             raise ValueError(
-                f"Journal data mismatch: {len(journal_data['levels'])} levels " +
-                "but expected 3")
+                f"Journal data mismatch: {len(journal_data['levels'])} levels "
+                + "but expected 3"
+            )
         elif journal_data["levels"][0] not in journal_data["data"].keys():
             raise KeyError(f"{journal_data['levels'][0]}")
     except TypeError as e:
@@ -37,9 +39,11 @@ def print_journal_headers(journal_data):
         raise TypeError(e) from e
 
     print(
-        f"Creating `journal.md` using data from {journal_data['data']['commit hash']}...")
+        f"Creating `journal.md` using data from {journal_data['data']['commit hash']}..."
+    )
     print(
-        f"Albion Online {journal_data['data']['type']} - {journal_data['data']['name']}")
+        f"Albion Online {journal_data['data']['type']} - {journal_data['data']['name']}"
+    )
     print(f"    first available on {journal_data['data']['date']}")
 
 
@@ -65,14 +69,20 @@ def create_journal_text(journal_data, templates):
         category_name = lib.escape_html(category_data["title"])
 
         # Count achievements in all subcategories
-        achievement_count = sum(len(subcat_data[level_2].values(
-        )) for subcat_data in category_data[level_1].values())
+        achievement_count = sum(
+            len(subcat_data[level_2].values())
+            for subcat_data in category_data[level_1].values()
+        )
 
         # Write category name with total achievement count in `journal.md` format
-        template_input = {"categoryID": category_id.lower(), "categoryName": category_name,
-                          "achievementCount": str(achievement_count)}
-        journal_text = "\n".join([journal_text,
-                                  templates["category_begin"].format(**template_input)])
+        template_input = {
+            "categoryID": category_id.lower(),
+            "categoryName": category_name,
+            "achievementCount": str(achievement_count),
+        }
+        journal_text = "\n".join(
+            [journal_text, templates["category_begin"].format(**template_input)]
+        )
 
         for subcat_data in category_data[level_1].values():
             # Use HTML encoding consistent with `journal.md` format
@@ -82,10 +92,13 @@ def create_journal_text(journal_data, templates):
             achievement_count = len(subcat_data[level_2].values())
 
             # Write subcategory name with achievement count in `journal.md` format
-            template_input = {"subcategoryName": subcategory_name,
-                              "achievementCount": str(achievement_count)}
-            journal_text = "\n".join([journal_text,
-                                      templates["subcategory_begin"].format(**template_input)])
+            template_input = {
+                "subcategoryName": subcategory_name,
+                "achievementCount": str(achievement_count),
+            }
+            journal_text = "\n".join(
+                [journal_text, templates["subcategory_begin"].format(**template_input)]
+            )
 
             for achievement_id, ach_data in subcat_data[level_2].items():
                 # Use HTML encoding consistent with `journal.md` format
@@ -106,20 +119,28 @@ def create_journal_text(journal_data, templates):
                     "rewardID": reward_id,
                     "reward": reward,
                     "requirementsNote": requirements_note,
-                    "requirementsList": ", ".join(requirements_list)
+                    "requirementsList": ", ".join(requirements_list),
                 }
                 if requirements_list:
                     # Include requirements with certain achievements
-                    journal_text = "\n".join([journal_text,
-                                              templates["achievement_with_requirements"].format(
-                                                  **template_input)])
+                    journal_text = "\n".join(
+                        [
+                            journal_text,
+                            templates["achievement_with_requirements"].format(
+                                **template_input
+                            ),
+                        ]
+                    )
                 else:
                     # Most achievements will not include their requirements
-                    journal_text = "\n".join([journal_text,
-                                              templates["achievement"].format(**template_input)])
+                    journal_text = "\n".join(
+                        [
+                            journal_text,
+                            templates["achievement"].format(**template_input),
+                        ]
+                    )
 
-            journal_text = "\n".join(
-                [journal_text, templates["subcategory_end"]])
+            journal_text = "\n".join([journal_text, templates["subcategory_end"]])
 
         journal_text = "\n".join([journal_text, templates["category_end"]])
 
